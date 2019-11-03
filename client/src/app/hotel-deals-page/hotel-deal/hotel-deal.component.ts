01fa58ee820644995ceed3194fe64b50883fc502
Rawan <rawan.nujoom@gmail.com>
2019-11-03 10:59:06 +0200

    add hotel-deals-page and single deal component

---
+++ client/src/app/hotel-deals-page/hotel-deal/hotel-deal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-deal',
+  templateUrl: './hotel-deal.component.html',
+  styleUrls: ['./hotel-deal.component.css']
+})
+export class HotelDealComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/hotel-deals-page/hotel-deals-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-deals-page',
+  templateUrl: './hotel-deals-page.component.html',
+  styleUrls: ['./hotel-deals-page.component.css']
+})
+export class HotelDealsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
