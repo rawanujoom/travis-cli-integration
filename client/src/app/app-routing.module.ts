16e1de887c0e3703c7c71966aca12e921fd05a0d
Rawan <rawan.nujoom@gmail.com>
2019-11-03 13:00:49 +0200

    implement hotel deals page filters and functionality

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HotelDealsPageComponent } from './hotel-deals-page/hotel-deals-page.component';
 
 const routes: Routes = [
+	{ path: '**', component: HotelDealsPageComponent },
 ];
 
 @NgModule({
--- client/src/app/hotel-deals-page/hotel-deals-page.component.ts
+++ client/src/app/hotel-deals-page/hotel-deals-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HotelDealsService } from '../services/hotel-deals.service';
+import { Options } from 'ng5-slider';
 
 @Component({
   selector: 'app-hotel-deals-page',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelDealsPageComponent implements OnInit {
 
-  constructor() { }
+  	deals:Array<Object> = [];
+	formFilters: any = {};
+	starRateOptions: Options = {};
+	guestRateOptions: Options = {};
 
-  ngOnInit() {
-  }
+	constructor(
+		private hotelDealsService: HotelDealsService
+	) { }
+
+	ngOnInit() {
+		this.initalizeFormFilters();
+		this.getHotelDeals({});
+	}
+
+	getHotelDeals(filters) {
+		this.hotelDealsService.getHotelDeals(filters).subscribe(data=> {
+			if (data['offers'] && data['offers']['Hotel'] && data['offers']['Hotel'].length > 0) {
+				this.deals = data['offers']['Hotel']; 
+			} else {
+				this.deals = [];
+			}
+		});
+	}
+
+	initalizeFormFilters() {
+		this.starRateOptions = this.hotelDealsService.getStarRateOptions();
+		this.guestRateOptions = this.hotelDealsService.getGuestRateOptions();
+		this.formFilters = {
+			destination 		: '',
+			minTripStartDate 	: null,
+			maxTripStartDate	: null,
+			lengthOfStay 		: null,
+			minTotalRate 		: null,
+			maxTotalRate 		: null,
+			minStarRating 		: this.starRateOptions.floor,
+			maxStarRating 		: this.starRateOptions.ceil,
+			minGuestRating 		: this.guestRateOptions.floor,
+			maxGuestRating 		: this.guestRateOptions.ceil
+		};
+	}
+
+	filterHotelDeals() {
+		let filters = {};
+		// add regular filters
+		for (let key in this.formFilters) {
+			if (this.formFilters[key]) {
+				filters[key] = this.formFilters[key];
+			}
+		};
+		
+		this.getHotelDeals(filters);
+	}
 
 }
