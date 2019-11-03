406e1f04aa910bd3d36e949907123fed7814ddf4
Rawan <rawan.nujoom@gmail.com>
2019-11-03 11:04:49 +0200

    add imports and declaration, use header component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { Ng5SliderModule } from 'ng5-slider';
+import { HotelDealsPageComponent } from './hotel-deals-page/hotel-deals-page.component';
+import { HotelDealComponent } from './hotel-deals-page/hotel-deal/hotel-deal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HotelDealsPageComponent,
+    HotelDealComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    Ng5SliderModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
