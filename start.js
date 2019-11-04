cf4573ca06868f216b7f6036e1b70e6d160bda52
Rawan <rawan.nujoom@gmail.com>
2019-11-04 16:51:45 +0200

    heroku port

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ var config = require('./config');
 var app = require('./app');
 
 /*Start server*/
-var server = app.listen(config.port, ()=>{
+var server = app.listen(process.env.PORT || config.port, ()=>{
     console.log('listening on port: ' + config.port);
 });
 
